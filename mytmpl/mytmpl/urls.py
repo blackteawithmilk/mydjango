ed8fde880694c3c48e47c65c5cc8f94bc9fb7526
company-pc <company-pc@bucunzaidewangzhan.com>
2018-04-04 09:19:25 +0800

    daily push

--- mytmpl/mytmpl/urls.py
+++ mytmpl/mytmpl/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views as myapp_views
 
 urlpatterns = [
+    path('', myapp_views.home, name="home"),
     path('admin/', admin.site.urls),
 ]
